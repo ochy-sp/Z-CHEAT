f1eafe9b407beda0a1615258de30222b502cd282
Ochy <ochydiscord@gmail.com>
2025-10-18 14:41:38 +0200

    Update zcheat.lua

--- zcheat.lua
+++ zcheat.lua
@@ -22,8 +22,8 @@ getgenv().AirHub = {}
 
 --// Load Modules
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub/main/Modules/Aimbot.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/AirHub/main/Modules/Wall%20Hack.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ochy-sp/z-cheat/refs/heads/main/AimBot.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ochy-sp/z-cheat/refs/heads/main/esp.lua"))()
 
 --// Variables
 
